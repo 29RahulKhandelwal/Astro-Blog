9d660566ca94093ace768258dcec5b64c73811ac
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2024-01-19 17:24:05 +0530

    hosting on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  output: "server",
+  output: "server"
   // image: {
   //   remotePatterns: [{ protocol: "https" }],
   //   domains: ["cdn.pixabay.com"],
   // },
+  ,
+  adapter: vercel()
 });
