3fca6fadada6b85c85d946edc2036aff58f9d28a
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2024-01-19 17:07:50 +0530

    prerender

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
+  output: "server",
   // image: {
   //   remotePatterns: [{ protocol: "https" }],
   //   domains: ["cdn.pixabay.com"],
